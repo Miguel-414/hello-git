d790eee2fcbec81d8e67e74fb25b445051190c69
Miguel-414 <mpachon414@gmail.com>
2024-02-27 11:32:20 -0500

    Metodo 2

--- hellogit2.py
+++ hellogit2.py
@@ -1,3 +1,4 @@
 import hellogit3 as h
 h.saludar()
 print(h.saludar())
+print(str(h.commit()))
--- hellogit3.py
+++ hellogit3.py
@@ -4,3 +4,13 @@ inicio1 = 'HELLOW'
 def saludar():
     print("hola")
     return "Hola git"
+ 
+def commit():
+    array = []
+    contador = 3
+    for _ in range(contador):
+       a = 1
+       b = a + 2
+    return array
+
+
